3248cac41b02e3063b6297b9415ba1a79058a01d
Xi Wei <xwei@handy.com>
2016-06-02 13:46:01 -0400

    Added deeplink project

---
+++ deeplink/src/main/java/com/xi_zz/deeplink/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xi_zz.deeplink;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		TextView textView = (TextView) findViewById(R.id.url);
+
+		textView.setText(getIntent().getDataString());
+	}
+}
