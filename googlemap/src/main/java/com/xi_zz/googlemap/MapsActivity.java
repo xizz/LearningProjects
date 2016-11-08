0ef5705d3305f3411d537106c181361f7d6bde41
Xi Wei <xwei@handy.com>
2016-11-08 11:15:51 -0500

    Using static MapView.

--- googlemap/src/main/java/com/xi_zz/googlemap/MapsActivity.java
+++ googlemap/src/main/java/com/xi_zz/googlemap/MapsActivity.java
@@ -2,30 +2,55 @@ package com.xi_zz.googlemap;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
+	private static MapView sMapView;
 
-	private GoogleMap mMap;
+	private ViewGroup mMapContainerLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maps);
-		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
-		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.map);
-		mapFragment.getMapAsync(this);
+
+		mMapContainerLayout = (ViewGroup) findViewById(R.id.mapContainer);
+		if (sMapView == null)
+		{
+			MapsInitializer.initialize(getApplicationContext());
+			sMapView = new MapView(getApplicationContext());
+			sMapView.getMapAsync(this);
+			sMapView.onCreate(null);
+		}
 	}
 
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		sMapView.onStart();
+		sMapView.onResume();
+		mMapContainerLayout.addView(sMapView);
+	}
+
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		mMapContainerLayout.removeAllViews();
+		sMapView.onPause();
+		sMapView.onStop();
+	}
 
 	/**
 	 * Manipulates the map once available.
@@ -39,11 +64,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 	@Override
 	public void onMapReady(GoogleMap googleMap)
 	{
-		mMap = googleMap;
+		GoogleMap map = googleMap;
 
 		// Add a marker in Sydney and move the camera
 		LatLng sydney = new LatLng(-34, 151);
-		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+		map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+		map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 	}
 }
