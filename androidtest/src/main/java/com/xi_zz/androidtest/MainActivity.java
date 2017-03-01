3fdfe93323187a479356104150737e2682859984
Xi Wei <xwei@handy.com>
2017-03-01 14:21:45 -0500

    created app to be tested

--- androidtest/src/main/java/com/xi_zz/androidtest/MainActivity.java
+++ androidtest/src/main/java/com/xi_zz/androidtest/MainActivity.java
@@ -2,14 +2,23 @@ package com.xi_zz.androidtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-
+	private TextView mDisplayText;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mDisplayText = (TextView) findViewById(R.id.display_text);
+	}
+
+	public void showMessage(View view)
+	{
+		mDisplayText.setText(R.string.hello_world);
 	}
 }
