3706fd8a67e1c535919982cbb654b3108ec3fb1c
Xi Wei <xwei@handy.com>
2017-03-24 16:45:22 -0400

    Completed BuildVariant module

--- buildvariant/src/main/java/com/xi_zz/buildvariant/MainActivity.java
+++ buildvariant/src/main/java/com/xi_zz/buildvariant/MainActivity.java
@@ -2,14 +2,17 @@ package com.xi_zz.buildvariant;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		TextView displayText = (TextView) findViewById(R.id.display_text);
+		displayText.setText(getString(R.string.product_flavor) + " " + getString(R.string.build_type));
 	}
 }
